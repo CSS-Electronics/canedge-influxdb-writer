9a118c1199dc8313806a293edbfd82e945e58bff
mfcss <mf@csselectronics.com>
2023-11-10 10:25:11 +0100

    Updated ARN layers to latest version of mdf-iter (required to load FW 01.07.05+ MFC/MFM files)

--- deploy-aws-lambda/arn-layers/README.md
+++ deploy-aws-lambda/arn-layers/README.md
@@ -3,9 +3,9 @@
 2. Open the Docker Desktop and wait until it's running
 3. Open your command prompt and run `docker pull public.ecr.aws/sam/build-python3.9`
 4. Install the [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html)
-5. Cofigure your AWS CLI via `aws configure`, providing your admin credentials and output type `json`
+5. Cofigure your AWS CLI via `aws configure --profile [your_profile_name]`, providing your admin credentials and output type `json`
 6. In Docker go to 'Settings/Resources/File Sharing', then add your new folder
-7. Run the file via `python build_layers.py` 
-8. If you've already built the zip file with dependencies, you can set this step to False in the code 
-
-Note: Some regions are not currently supported.
+7. Update the information in `python build_layers.py` to fit your requirements (note that you can only create layers for regions that are accessible via your AWS account)
+8. Once updated, start by running the script with `run_req_build = True` to create the dependencies via Docker and the script 
+9. Next, zip the resulting `python/` folder into a zip file named accordingly to match the name in the script 
+10. Set `run_req_build = False` and run the script again to start publishing the layers to the regions you've specified
--- deploy-aws-lambda/arn-layers/build_layers.py
+++ deploy-aws-lambda/arn-layers/build_layers.py
@@ -1,24 +1,29 @@
+# about: This script lets you build ARN layers based on the requirements.txt next to the script - see the separate README guide
+
 # requires Docker to be running
 import os, json, sys
 import subprocess
 
 # specify base details and region list
-layer_name = "canedge-influxdb-writer-2"
+profile_name = "admin_2022"
+layer_name = "canedge-influxdb-writer-v3"
+zip_name = "canedge-influxdb-writer.zip"
 layer_description = "CSS Electronics canedge-influxdb-writer dependencies for use in AWS Lambda functions"
 csv_path = "lambda_layer_arns.csv"
-run_req_build = True
+run_req_build = False
+update_csv_file = True
 
-    # not working:
-    # "cn-north-1",
-    # "cn-northwest-1",
-    # "eu-central-2",
-    # "eu-south-1",
-    # "eu-south-2",
-    # "me-south-1",
-    # "me-central-1",
-    # "us-gov-east-1",
-    # "us-gov-west-1",
-    # "ap-east-1"
+# not included in the prebuilt list :
+# "cn-north-1",
+# "cn-northwest-1",
+# "eu-central-2",
+# "eu-south-1",
+# "eu-south-2",
+# "me-south-1",
+# "me-central-1",
+# "us-gov-east-1",
+# "us-gov-west-1",
+# "ap-east-1"
 
 regions = [
     "ap-northeast-1",
@@ -40,6 +45,7 @@ regions = [
     "us-west-2",
 ]
 
+# -----------------------------------------------
 # create zip with requirements.txt dependencies
 if run_req_build:
     os.system("rmdir /S/Q python")
@@ -48,32 +54,33 @@ if run_req_build:
         'docker run -v "%cd%":/var/task "public.ecr.aws/sam/build-python3.9" /bin/sh -c "pip install -r requirements.txt -t python/lib/python3.9/site-packages/; exit"'
     )
     os.system("rmdir /S/Q python\\lib\\python3.9\\site-packages\\botocore")
-    os.system("zip -r canedge-influxdb-writer.zip python")
+    print(
+        f"\nCompleted building dependencies, now exiting script.\nNext, manually zip the python/ folder to '{zip_name}', then set run_req_build = False and run the script again"
+    )
+    sys.exit()
 
+# -----------------------------------------------
 # for each region, publish AWS layer with build zip
 region_arn_list = []
 
 for region in regions:
-    print(f"executing region {region}")
+    print(f"\n------------------------------\nExecuting region {region}")
 
     # create the layers
     arn_output = subprocess.check_output(
-        f'aws lambda publish-layer-version --region {region} --layer-name {layer_name} --description "{layer_description}" --cli-connect-timeout 6000 --license-info "MIT" --zip-file "fileb://canedge-influxdb-writer.zip" --compatible-runtimes python3.9',
+        f'aws --profile admin_2022 lambda publish-layer-version --region {region} --layer-name {layer_name} --description "{layer_description}" --cli-connect-timeout 6000 --license-info "MIT" --zip-file "fileb://{zip_name}" --compatible-runtimes python3.9',
         shell=True,
     ).decode("utf-8")
-
-    print("We build the layer", arn_output)
 
     version = int(json.loads(arn_output)["Version"])
 
     # make them public
     make_public = subprocess.check_output(
-        f"aws lambda add-layer-version-permission --layer-name {layer_name} --version-number {version} --statement-id allAccountsExample2 --principal * --action lambda:GetLayerVersion --region {region}",
+        f"aws --profile {profile_name} lambda add-layer-version-permission --layer-name {layer_name} --version-number {version} --statement-id allAccountsExample2 --principal * --action lambda:GetLayerVersion --region {region}",
         shell=True,
     )
 
     print("Build layer:", arn_output)
-
     print("Make public:", make_public)
 
     arn = str(json.loads(arn_output)["LayerVersionArn"])
@@ -81,10 +88,13 @@ for region in regions:
     region_arn_list.append(region_arn)
 
 # write data to CSV
-output_file = open(csv_path, "w")
-for region_arn in region_arn_list:
-    output_file.write(region_arn)
+if update_csv_file:
+    output_file = open(csv_path, "w")
+    for region_arn in region_arn_list:
+        output_file.write(region_arn)
 
-output_file.close()
+    output_file.close()
 
-print(f"Completed writing {len(region_arn_list)} out of {len(regions)} to CSV {csv_path}")
+    print(
+        f"Completed writing {len(region_arn_list)} out of {len(regions)} to CSV {csv_path}"
+    )
